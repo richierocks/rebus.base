88cf827584e0c26238522ae89ec64e94027d30be
Richard J. Cotton <rjc2003@wcmcq.net>
2015-12-14 14:40:43 +0300

    deprecated %c%

--- R/concatenation.R
+++ R/concatenation.R
@@ -1,22 +1,21 @@
 #' Combine strings together
-#' 
+#'
 #' Operator equivalent of \code{regex}.
-#' 
+#'
 #' @param x A character vector.
 #' @param y A character vector.
 #' @return A character vector representing part or all of a regular expression.
 #' @note \code{\%c\%} was the original operator for this ('c' for
 #' 'concatenate').  This is hard work to type on a QWERTY keyboard
-#' though, so \code{\%R\%} is now preferred.
+#' though, so it has been replaced with \code{\%R\%}.
 #' @seealso \code{\link{regex}}, \code{\link[base]{paste}}
 #' @examples
 #' letters %R% LETTERS
-#' letters %c% LETTERS
 #' @name Concatenation
 #' @export
 `%c%` <- function(x, y)
 {
-  # .Deprecated("%R%")
+  .Deprecated("%R%")
   regex(x, y)
 }
 
